5ae9e56074492486e9d6afc4394390bf8fafca1c
Bablu Mia <bablumiah2000@gmail.com>
2022-05-10 10:20:19 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div >
-      <h2>Hlw from app</h2>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/Shared/Navbar.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const menuItem = (
+    <>
+      <li className="mx-2">
+        <Link to={'/'} className='hover:bg-[#3A4256] hover:text-white rounded-lg'>HOME</Link>
+      </li>
+      
+      <li className="mx-2">
+        <Link to={'/apoinment'} className='hover:bg-[#3A4256] hover:text-white rounded-lg'>APOINMENT</Link>
+      </li>
+      <li className="mx-2">
+        <Link to={'/review'} className='hover:bg-[#3A4256] hover:text-white rounded-lg'>REVIEW</Link>
+      </li>
+      <li className="mx-2">
+        <Link to={'/contact'} className='hover:bg-[#3A4256] hover:text-white rounded-lg'>CONTACT US</Link>
+      </li>
+      <li className="mx-2">
+        <Link to={'/about'} className='hover:bg-[#3A4256] hover:text-white rounded-lg'>ABOUT</Link>
+      </li>
+      <li className="mx-2">
+        <Link to={'/login'} className='hover:bg-[#3A4256] hover:text-white rounded-lg'>LOGIN</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItem}
+          </ul>
+        </div>
+        <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <ul class="menu menu-horizontal mr-8">
+          {menuItem}
+        </ul>
+      </div>
+      {/* <div class="navbar-end">
+        <a class="btn">Get started</a>
+      </div> */}
+    </div>
+  );
+};
+
+export default Navbar;
