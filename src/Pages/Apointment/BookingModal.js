bce5c4550f66bfdf051dfbe6cee72daac982768b
Bablu Mia <bablumiah2000@gmail.com>
2022-05-14 01:34:51 +0600

    added

--- src/Pages/Apointment/BookingModal.js
+++ src/Pages/Apointment/BookingModal.js
@@ -1,24 +1,52 @@
 import { format } from "date-fns";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import swal from "sweetalert";
+import auth from "../../firebase.init";
 
 const BookingModal = ({ treatment, date, setTreatment }) => {
+  const { _id, name, slots } = treatment;
+  const [user] = useAuthState(auth);
+  const formattedDate = format(date, "PP");
   const handleBooking = (event) => {
     event.preventDefault();
     const slot = event.target.slot.value;
-    const Userame = event.target.name.value;
-    const email = event.target.email.value;
     const number = event.target.number.value;
-    const data = {
-      name: treatment.name,
-      slot: slot,
-      Userame: Userame,
-      email: email,
+    const booking = {
+      treatmentId: _id,
+      treatment: name,
+      date: formattedDate,
+      slot,
+      patientName: user?.displayName,
+      patient: user?.email,
       number: number,
     };
-    console.log(data);
-    event.target.reset();
-    setTreatment(null);
+    console.log(booking);
+    // event.target.reset();
+
+    fetch('http://localhost:5000/booking', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(booking)
+    })
+
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setTreatment(null);
+        swal({
+          title: "Booking Notification",
+          text: `Succesfully booked An  Apointment By ${user.displayName} At ${slot}`,
+          icon: "success",
+        });
+      });
   };
+
+
+ 
+
   return (
     <div className="mx-auto">
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -31,7 +59,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
             ✕
           </label>
           <h3 className="font-bold text-lg text-center mb-3">
-            Booking For : {treatment.name}
+            Booking For : {name}
           </h3>
           <form
             onSubmit={handleBooking}
@@ -45,7 +73,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
               disabled
             />
             <select name="slot" className="select select-bordered w-full ">
-              {treatment.slots.map((slot) => (
+              {slots.map((slot) => (
                 <option value={slot} key={slot}>
                   {slot}
                 </option>
@@ -54,13 +82,14 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
             <input
               type="text"
               name="name"
-              placeholder="Your Name"
               className="input w-full  input-bordered"
+              value={user?.displayName}
+              disabled
             />
             <input
               type="email"
               name="email"
-              placeholder="Your Email"
+              value={user?.email}
               className="input w-full   input-bordered"
             />
             <input
